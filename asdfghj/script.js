522381f1b71b3cd53f83219ca5135a7cb83eaab4
Abderaheman <abdarahmancherive95@gmail.com>
2024-01-16 09:31:16 +0000

    m

---
+++ asdfghj/script.js
@@ -0,0 +1,8 @@
+<script src="src/script.js"></script>
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
